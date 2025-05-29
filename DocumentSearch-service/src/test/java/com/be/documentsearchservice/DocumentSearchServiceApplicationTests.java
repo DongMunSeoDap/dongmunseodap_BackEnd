8f85a266a70789b2e4f49e2370010b398cf90db1
pjuyong <imjuyongp@gmail.com>
2025-05-29 17:32:09 +0900

    Initial commit

---
+++ DocumentSearch-service/src/test/java/com/be/documentsearchservice/DocumentSearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.be.documentsearchservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentSearchServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ eureka-server/src/test/java/com/be/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.be.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
