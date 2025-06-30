0cc4462488ca608f911098480ffaf174600cd742
pjuyong <imjuyongp@gmail.com>
2025-06-30 23:53:02 +0900

    AWS S3 의존성 변경

---
+++ DocumentUpload-service/src/main/java/com/be/documentuploadservice/global/config/S3Config.java
@@ -0,0 +1,14 @@
+package com.be.documentuploadservice.global.config;
+
+import lombok.Getter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Configuration
+public class AwsConfig {
+  
+
+
+}
+
