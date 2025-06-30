099ac3327d2224dc7a9155d20c1a9e51f98f17de
pjuyong <imjuyongp@gmail.com>
2025-07-01 01:43:54 +0900

    업로드 시각 추가

--- DocumentUpload-service/src/main/java/com/be/documentuploadservice/dto/response/UploadResponse.java
+++ DocumentUpload-service/src/main/java/com/be/documentuploadservice/dto/response/UploadResponse.java
@@ -1,6 +1,7 @@
 package com.be.documentuploadservice.dto.response;
 
 import com.be.documentuploadservice.entity.UplaodStatus;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -18,16 +19,15 @@ public class UploadResponse {
 
   private String documentName;
 
-  private String s3Path;
+  private String s3Path; // s3 저장 경로
 
-  private String uploadedAt;
+  private LocalDateTime uploadedAt;
 
   private String uploadedBy;
 
   private UplaodStatus status; // 서버 내부처리 결과
 
   private String message; // FAILURE시 오류 메세지 출력
-
 
   private String pdfUrl; // s3 버킷 안에 있는 객체 url
 
