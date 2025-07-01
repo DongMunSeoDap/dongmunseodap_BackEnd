4100c46aa70fde4a87721fa2d92ce39e777d7f56
pjuyong <imjuyongp@gmail.com>
2025-07-02 02:40:51 +0900

    문서 전체 조회 로직 추가

--- DocumentUpload-service/src/main/java/com/be/documentuploadservice/resolver/DocumentUploadResolver.java
+++ DocumentUpload-service/src/main/java/com/be/documentuploadservice/resolver/DocumentUploadResolver.java
@@ -7,9 +7,12 @@ import com.be.documentuploadservice.entity.PathName;
 import com.be.documentuploadservice.global.reponse.BaseResponse;
 import com.be.documentuploadservice.service.DocumentUploadService;
 import graphql.schema.DataFetchingEnvironment;
+import java.nio.file.Path;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.web.multipart.MultipartFile;
 
 
@@ -18,17 +21,10 @@ public class DocumentUploadResolver {
 
   private final DocumentUploadService documentUploadService;
 
-  @MutationMapping
-  public BaseResponse<UploadResponse> uploadDocument(
-      @Argument PathName pathName,
-      @Argument MultipartFile file,
-      @Argument DocumentMetaInput meta,
-      @Argument EventContextInput context,
-      DataFetchingEnvironment env) {
-
-    UploadResponse uploadResponse = documentUploadService.uploadDocuments(pathName, file);
-
-    return BaseResponse.success("Upload Success", uploadResponse);
+  // S3에 업로드 된 파일 전체 조회
+  @QueryMapping
+  public List<String> getAllFiles(@Argument PathName pathName) {
+    return documentUploadService.getAllFiles(pathName);
   }
 
 }
