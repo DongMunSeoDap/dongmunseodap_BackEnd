c8308ddd3c79a5b488f196fcbf9687a0caf26265
Jihyeon02 <ruby0656@suwon.ac.kr>
2025-07-11 17:47:13 +0900

    0711: 테스트 성공할 수 있게 리팩토링

--- DocumentSearch-service/src/main/java/com/be/documentsearchservice/service/EmbeddingService.java
+++ DocumentSearch-service/src/main/java/com/be/documentsearchservice/service/EmbeddingService.java
@@ -8,9 +8,12 @@ import io.pinecone.clients.Index;
 import io.pinecone.configs.PineconeConfig;
 import io.pinecone.configs.PineconeConnection;
 import io.pinecone.unsigned_indices_model.QueryResponseWithUnsignedIndices;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -19,7 +22,7 @@ public class EmbeddingService {
     private final EmbeddingModel embeddingClient;
     private EmbeddingService embeddingService;
     private EmbeddingResponse embeddingResponse;
-
+    private static final Logger log = LoggerFactory.getLogger(EmbeddingService.class);
     public EmbeddingService(EmbeddingModel embeddingClient) {
         this.embeddingClient = embeddingClient;
     }
@@ -35,6 +38,7 @@ public class EmbeddingService {
         if (input == null) {
             throw new IllegalArgumentException("Embedding is null. Did you call /embedding first?");
         }
+        log.info(">>> first 5 values = {}", Arrays.toString(Arrays.copyOf(input, 5)));
         PineconeConfig config = new PineconeConfig("pcsk_58CvBb_U2dfMdm48R9ayMTLRZ9E5Zn1xhf4GJ7wK1vWu1BWVKUHKRsbeVXExWbhzo73sYq");
         config.setHost("https://dongmunseodap-document-index-dev-1a3x1qg.svc.aped-4627-b74a.pinecone.io");
         PineconeConnection connection = new PineconeConnection(config);
