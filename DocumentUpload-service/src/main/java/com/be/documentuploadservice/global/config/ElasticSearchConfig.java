2c9780c06d74814adb2223cb1dd08ef85f41b8b4
Juyong Park <imjuyongp@gmail.com>
2025-08-04 03:53:32 +0900

    es repository 활성화

--- DocumentUpload-service/src/main/java/com/be/documentuploadservice/global/config/ElasticSearchConfig.java
+++ DocumentUpload-service/src/main/java/com/be/documentuploadservice/global/config/ElasticSearchConfig.java
@@ -1,14 +1,17 @@
 package com.be.documentuploadservice.global.config;
 
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Configuration
+@EnableElasticsearchRepositories // Elasticsearch Repository 활성화를 위한 어노테이션
 public class ElasticSearchConfig extends ElasticsearchConfiguration {
 
-  @Value("${spring.elasticsearch.uris")
+  @Value("${spring.elasticsearch.username}")
   private String username;
 
   @Value("${spring.elasticsearch.password}")
