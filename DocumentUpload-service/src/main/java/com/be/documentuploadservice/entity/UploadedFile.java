99540a0801cc1bd61347c1dfb49886a14c8a8916
Juyong Park <imjuyongp@gmail.com>
2025-09-02 19:41:07 +0900

    rename: entity class 이름 변경(변수중복)

--- DocumentUpload-service/src/main/java/com/be/documentuploadservice/entity/UploadedFile.java
+++ DocumentUpload-service/src/main/java/com/be/documentuploadservice/entity/UploadedFile.java
@@ -24,7 +24,7 @@ import java.time.LocalDateTime;
 @Setter
 @Document(indexName = "file_meta") // 인데스 이름
 
-public class File {
+public class File { // es 저장
 
   @Id
   @Field(type = FieldType.Long)
@@ -38,24 +38,10 @@ public class File {
   private String s3Key; // S3 객체 키
 
   @Field(type = FieldType.Text)
-  private String s3Path; // s3 객체 경로
-
-  @Field(type = FieldType.Text)
   private String uploadedBy; // 업로드 한 사람(추후에 user와 매핑)
 
   @Field(type = FieldType.Date)
   private LocalDateTime uploadedAt; // 절대 시점
-
-  // 문서 메타 정보
-  @Field(type = FieldType.Text)
-  private String mimeType; // 확장자
-
-  @Field(type = FieldType.Text)
-  private String language; // 언어
-
-  // 벡터DB와 연동을 위한 필드
-  @Field(type = FieldType.Text)
-  private String nameSpace; // 민혁님이 정한 namespace 기준과 동일
 
   // 업로드 상태
   @Field(type = FieldType.Keyword)
