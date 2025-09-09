7724590f5e9890e8058f936cd895b4c208fb6b77
imjuyongp <imjuyongp@gmail.com>
2025-09-09 17:48:06 +0900

    refactor: 오타 수정

--- DocumentUpload-service/src/main/java/com/be/documentuploadservice/repository/FileElasticSearchRepository.java
+++ DocumentUpload-service/src/main/java/com/be/documentuploadservice/repository/FileElasticSearchRepository.java
@@ -4,8 +4,6 @@ import com.be.documentuploadservice.entity.UploadedFile;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
-import java.lang.ScopedValue;
-
 @Repository
 public interface FileElasticSearchRepository extends ElasticsearchRepository<UploadedFile, Long> {
 
