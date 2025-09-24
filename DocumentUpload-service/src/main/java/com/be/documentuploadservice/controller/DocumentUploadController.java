dba39d0f04807416917889b6ff633bceda41f8fb
imjuyongp <imjuyongp@gmail.com>
2025-09-24 15:21:47 +0900

    endpoint prefix 수정

--- DocumentUpload-service/src/main/java/com/be/documentuploadservice/controller/DocumentUploadController.java
+++ DocumentUpload-service/src/main/java/com/be/documentuploadservice/controller/DocumentUploadController.java
@@ -20,7 +20,7 @@ import org.springframework.web.multipart.MultipartFile;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/upload")
 public class DocumentUploadController {
 
   private final DocumentUploadService documentUploadService;
